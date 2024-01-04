522694155adfd12bbd29df3ed75e1056c99f151f
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-01-04 12:10:36 +0600

    Fix a variable's name

--- src/singleResponsibilityPrinciple/example_1/Order.java
+++ src/singleResponsibilityPrinciple/example_1/Order.java
@@ -2,12 +2,12 @@ package singleResponsibilityPrinciple.example_1;
 
 public class Order {
     String customerName;
-    String purchaseItem;
-    int purchaseQuantuty;
+    String purchasedItem;
+    int purchasedQuantity;
 
     public Order(String customerName, String purchaseItem, int purchaseQuantuty) {
         this.customerName = customerName;
-        this.purchaseItem = purchaseItem;
-        this.purchaseQuantuty = purchaseQuantuty;
+        this.purchasedItem = purchaseItem;
+        this.purchasedQuantity = purchaseQuantuty;
     }
 }
