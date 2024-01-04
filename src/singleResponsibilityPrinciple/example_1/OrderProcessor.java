ab61e055e496bed6556e236132e0204db5f159df
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-01-04 12:40:28 +0600

    Set the module that processes orders. If the order is correctly formed, he saves it to the database and sends an email to confirm the order. It is better to divide the module into three separate ones, each of which will perform a single function

---
+++ src/singleResponsibilityPrinciple/example_1/OrderProcessor.java
@@ -0,0 +1,11 @@
+package singleResponsibilityPrinciple.example_1;
+
+public class OrderProcessor {
+    public void process(Order order) {
+        MyOrderRepository repository = new MyOrderRepository();
+        ConfirmationEmailSender emailSender = new ConfirmationEmailSender();
+        if (repository.save(order)) {
+            emailSender.sendConfirmationEmail(order);
+        }
+    }
+}
